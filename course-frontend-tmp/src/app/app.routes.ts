720a8d3b5ea9df9f70c1f9f4619b9ab71374c1a6
amaljerry <amaljerry666@gmail.com>
2025-12-02 18:09:38 +0530

    Remove course-frontend-tmp folder

--- course-frontend-tmp/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { CourseListComponent } from './components/course-list/course-list.component';
-import { EnrollmentListComponent } from './components/enrollment-list/enrollment-list.component';
-
-export const appRoutes: Routes = [
-  { path: '', component: CourseListComponent },        // student page
-  { path: 'teacher', component: EnrollmentListComponent }, // teacher page
-  { path: '**', redirectTo: '' },
-];
